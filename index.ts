f931f35b44fa0a18f5b62f21a5259e083ad82728
hahagarden <tingyuan413@gmail.com>
2022-09-21 01:03:42 +0900

    types: unknown, void, never

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 type Player = {
-  readonly name: string; //readonly
+  readonly name: string;
   age?: number;
 };
 
@@ -7,7 +7,7 @@ const player: { name: string; age?: number } = { name: "garden" };
 const playerTuple: [string, number, boolean] = ["Nico", 12, true];
 const numbers: any[] = [1, 2, 3];
 const booleans: any = true;
-console.log(numbers + booleans); //any, occur no error
+console.log(numbers + booleans);
 
 function playerMaker(name: string): Player {
   return { name };
@@ -15,4 +15,17 @@ function playerMaker(name: string): Player {
 
 const nico = playerMaker("Nico");
 nico.age = 12;
-nico.name = "Nicolas"; //readonly error
+nico.name = "Nicolas";
+
+let data: unknown; // type unknown
+if (typeof data === "string") console.log(data);
+if (typeof data === "number") console.log(data + 1);
+
+function thisIsVoid() {
+  //type void
+  console.log("my type");
+}
+
+function hello(): never {
+  throw new Error("xxx");
+} // return 하지 않고 error 발생시킬 때
