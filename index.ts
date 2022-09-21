f80d561afa97c4f023cc3ab0ca8366f87db7833f
hahagarden <tingyuan413@gmail.com>
2022-09-21 13:05:34 +0900

    call signature, overloading

--- index.ts
+++ index.ts
@@ -1,31 +1,10 @@
-type Player = {
-  readonly name: string;
-  age?: number;
+type Add = {
+  //call signiture, overloading: multiple call signiture
+  (a: number, b: number): number;
+  (a: number, b: string): number;
 };
 
-const player: { name: string; age?: number } = { name: "garden" };
-const playerTuple: [string, number, boolean] = ["Nico", 12, true];
-const numbers: any[] = [1, 2, 3];
-const booleans: any = true;
-console.log(numbers + booleans);
-
-function playerMaker(name: string): Player {
-  return { name };
-}
-
-const nico = playerMaker("Nico");
-nico.age = 12;
-nico.name = "Nicolas";
-
-let data: unknown; // type unknown
-if (typeof data === "string") console.log(data);
-if (typeof data === "number") console.log(data + 1);
-
-function thisIsVoid() {
-  //type void
-  console.log("my type");
-}
-
-function hello(): never {
-  throw new Error("xxx");
-} // return 하지 않고 error 발생시킬 때
+const add: Add = (a, b) => {
+  if (typeof b === "string") return a;
+  else return a + b;
+};
