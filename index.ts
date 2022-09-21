cd407c09dad12366c719aea2dbb7e81d7aa4164a
hahagarden <tingyuan413@gmail.com>
2022-09-21 13:27:11 +0900

    polymorphism

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-type Add = {
-  //call signiture, overloading: multiple call signiture
-  (a: number, b: number): number;
-  (a: number, b: string): number;
+type Print = {
+  (arr: number[]): void;
+  (arr: string[]): void;
+  (arr: boolean[]): void;
+  (arr: (number | boolean)[]): void;
+  (arr: (string | boolean)[]): void;
+  (arr: (number | string)[]): void;
+}; //polymorphism(diverse structure)
+
+const printArr: Print = (arr) => {
+  arr.forEach((i) => console.log(i));
 };
 
-const add: Add = (a, b) => {
-  if (typeof b === "string") return a;
-  else return a + b;
-};
+printArr([1, 2, 3, 4]);
+printArr([1, 2, true, false]);
