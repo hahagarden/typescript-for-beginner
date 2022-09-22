361da113732e416d60fec5d248b9c4f0649adbcd
hahagarden <tingyuan413@gmail.com>
2022-09-22 22:53:06 +0900

    abstract class abstract method

--- index.ts
+++ index.ts
@@ -2,17 +2,24 @@ abstract class User {
   constructor(
     private firstName: string,
     private lastName: string,
-    public nickName: string
+    private nickName: string
   ) {}
+
+  abstract getNickName(arg: string): void; //abtract method should have call-signature and no implementation
+
   getFullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
 
-class Player extends User {}
+class Player extends User {
+  getNickName(arg: string): void {
+    console.log(this.nickName); //cannot access private nickName
+  }
+} //should implement abstract method getNickName()
 
 const nico = new Player("Nico", "las", "니꼬");
 nico.getFullName();
 
-nico.nickName = "라스";
+nico.nickName = "라스"; //cannot access private nickName
 console.log(nico);
