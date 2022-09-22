2f8a6ab3f93b9e568e12b30b51a1a60213b00abd
hahagarden <tingyuan413@gmail.com>
2022-09-22 22:38:28 +0900

    abstract class method

--- index.ts
+++ index.ts
@@ -4,12 +4,15 @@ abstract class User {
     private lastName: string,
     public nickName: string
   ) {}
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 class Player extends User {}
 
 const nico = new Player("Nico", "las", "니꼬");
-const garden = new User("haha", "garden", "정원"); //abtract class 상속받을 수만 있는 클래스, 직접 인스턴스 생성 불가
+nico.getFullName();
 
 nico.nickName = "라스";
 console.log(nico);
