aab60c96379491dc29149a43ea46084261251985
hahagarden <tingyuan413@gmail.com>
2022-09-23 12:52:52 +0900

    interface inheritance vs. type inheritance

--- index.ts
+++ index.ts
@@ -1,21 +1,15 @@
-type Health = number;
-type Nickname = string;
-type Team = "red" | "blue" | "green";
-type Friends = Array<string>;
-interface Player {
+type User = {
   name: string;
-  age: number;
-  health: Health;
-  nickname: Nickname;
-  team: Team;
-  friends: Friends;
-} //interface. almost same as type, but it is only for shaping 'object'
-
+};
+type Player = User & {}; //type inheritance
 const nico: Player = {
   name: "Nico",
-  age: 20,
-  health: 100,
-  nickname: "니꼬",
-  team: "green",
-  friends: ["haha", "garden"],
+};
+
+interface Pet {
+  name: string;
+}
+interface PlayerPet extends Pet {} //interface inheritance
+const bubu: PlayerPet = {
+  name: "bubu",
 };
