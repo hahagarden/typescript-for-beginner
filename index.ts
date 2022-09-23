ac729b09fddfc74696c67ff0e0198f96b617806d
hahagarden <tingyuan413@gmail.com>
2022-09-23 12:38:30 +0900

    type Alias

--- index.ts
+++ index.ts
@@ -1,29 +1,21 @@
-type Words = {
-  [y: string]: string;
-}; //means property key(y) has string type
+type Health = number;
+type Nickname = string;
+type Team = "red" | "blue" | "green";
+type Friends = Array<string>;
+type Player = {
+  name: string;
+  age: number;
+  health: Health;
+  nickname: Nickname;
+  team: Team;
+  friends: Friends;
+}; //type Alias
 
-class Dict {
-  private words: Words; //not immediately initialized in the constructor, initialize words manually
-  constructor() {
-    this.words = {};
-  }
-  add(word: Word) {
-    //class Word can also be type
-    if (this.words[word.term] === undefined) {
-      //if property do not exist, return undefined
-      this.words[word.term] = word.def; //undefined property 동적 생성([프로퍼티 키 표현식] 대괄호 표기법)
-    }
-  }
-  def(term: string) {
-    return this.words[term];
-  }
-}
-
-class Word {
-  constructor(public readonly term: string, public readonly def: string) {}
-}
-
-const kimchi = new Word("kimchi", "한국음식");
-const dict = new Dict();
-dict.add(kimchi);
-dict.def("kimchi");
+const nico: Player = {
+  name: "Nico",
+  age: 20,
+  health: 100,
+  nickname: "니꼬",
+  team: "green",
+  friends: ["haha", "garden"],
+};
