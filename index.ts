b6c74b8f56d0adf44bd75084a40d1dc50fddfd43
hahagarden <tingyuan413@gmail.com>
2022-09-23 13:26:19 +0900

    abstract is important
    부모추상클래스에서 추상메소드의 형태를 정해놓으면
    자식클래스들이 그 추상메소드를 꼭 구현해야 함. 강제함.
    수많은 Player의 형식이 표준화될 수 있음

--- index.ts
+++ index.ts
@@ -1,26 +1,14 @@
-type User = {
-  name: string;
-};
-type Player = User & {}; //type inheritance
-const nico: Player = {
-  name: "Nico",
-};
+abstract class User {
+  constructor(protected firstName: string, protected lastName: string) {}
+  abstract sayHi(name: string): string;
+  abstract fullName(): string;
+}
 
-interface Pet {
-  name: string;
-}
-interface Pet {
-  //same interface Pet
-  animal: string;
-}
-interface PlayerPet extends Pet {}
-interface PlayerPet {
-  //same interface PlayerPet
-  age: number;
-}
-const bubu: PlayerPet = {
-  //interfaces merged
-  name: "bubu",
-  animal: "dog",
-  age: 2,
-};
+class Player extends User {
+  sayHi(name: string) {
+    return `Hi ${name}, I am ${this.firstName} ${this.lastName}`;
+  }
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+} //abstract class is important. It forces subclasses to implement abstract methods so that they all have standard shape.
