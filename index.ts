25a358d9ef76974384a10415f8063ffd846642f2
hahagarden <tingyuan413@gmail.com>
2022-09-23 13:49:46 +0900

    interface is like abstract class *
    interface는 클래스가 아니지만 클래스의 형태를 특정할 수 있게 해주는 방법. type alias와 비슷하게 오브젝트의 모양을 구현해주는 방법이지만, implements로 클래스에도(클래스처럼) 사용할 수 있는 것.
    
    interface not compiled into Javascript.
    인터페이스를 클래스에 구현하면 추상클래스 추상메소드처럼 프로퍼티와 메소드를 꼭 구현하도록 강제할 수 있음.
    그러나 자바스크립트로 컴파일되지 않아서 파일용량을 줄일 수 있음.
    private, protected은 사용 못함.
    경우에 따라 interface와 type, abstract-class를 알맞게 사용.

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-abstract class User {
-  constructor(protected firstName: string, protected lastName: string) {}
-  abstract sayHi(name: string): string;
-  abstract fullName(): string;
-}
+interface User {
+  firstName: string;
+  lastName: string;
+  sayHi(name: string): string;
+  fullName(): string;
+} //interface is not compiled into JS, it reduces file size.
 
-class Player extends User {
+class Player implements User {
+  constructor(
+    public firstName: string, //cannot use private, protected protection-level.
+    public lastName: string
+  ) {}
   sayHi(name: string) {
     return `Hi ${name}, I am ${this.firstName} ${this.lastName}`;
   }
   fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
-} //abstract class is important. It forces subclasses to implement abstract methods so that they all have standard shape.
+}
